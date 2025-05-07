1d5019fa15f3256c56ff4afc03a1a8ffbf9cb5c1
Yugsingh05 <yugsingh7622@gmail.com>
2025-05-07 18:46:38 +0530

    feat : added the styles to this form the tailwind

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-6'>page</div>
+  )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 export default function Home() {
   return (
-    <div className="text-tealLight bg-incoming border border-conversationBorder grid grid-cols-main p-4">
-      <div className="bg-chat h-48 rounded text-primaryStrong">
-        This uses simplified Tailwind classes!
-      </div>
-    </div>
+   <div className="text-teal-light">Home</div>
   );
 }
