4dd78041a4cd4643d7b5d89625bcc62890e9335a
Yugsingh05 <yugsingh7622@gmail.com>
2025-05-11 18:24:55 +0530

    chat compoent UI complete

--- src/components/Chat/ChatContainer.tsx
+++ src/components/Chat/ChatContainer.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
 const ChatContainer = () => {
-  return (
-    <div>ChatContainer</div>
+  return ( 
+    <div className='h-[80vh] w-full relative flex-grow overflow-auto custom-scrollbar'>
+        <div className='bg-chat-background bg-fixed h-full w-full opacity-7 fixed left-0 top-0 z-0 '>
+         </div>
+
+         <div className="flex w-full">
+            <div className="flex flex-col justify-end w-full gap-1 overflow-auto"></div>
+         </div>
+    </div>
   )
 }
 
--- src/components/Chat/MessageBar.tsx
+++ src/components/Chat/MessageBar.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { BsEmojiSmile } from "react-icons/bs";
+import { ImAttachment } from "react-icons/im";
+import { MdSend } from "react-icons/md";
+import {  FaMicrophone } from "react-icons/fa";
 
 const MessageBar = () => {
   return (
-    <div>MessageBar</div>
-  )
-}
+    <div className="bg-panel-header-background h-20 px-4 flex items-center gap-6 relative">
+      <>
+        <div className="flex gap-6">
+          <BsEmojiSmile
+            className="text-panel-header-icon text-xl cursor-pointer"
+            title="Emoji"
+          />
+          <ImAttachment
+            className="text-panel-header-icon text-xl cursor-pointer"
+            title="Attachment"
+          />
+        </div>
+        <div className="w-full rounded-lg h-10 flex items-center">
+          <input
+            type="text"
+            placeholder="Type a message"
+            className="bg-input-background text-start focus:outline-none text-white h-10 rounded-lg px-5 w-full"
+          />
+        </div>
 
-export default MessageBar
+        <div className="flex w-10 items-center justify-center">
+            <button>
+                <MdSend className="text-panel-header-icon text-xl cursor-pointer" title="Send" />
+                {/* <FaMicrophone className="text-panel-header-icon text-xl cursor-pointer" title="Record" /> */}
+            </button>
+        </div>
+      </>
+    </div>
+  );
+};
+
+export default MessageBar;
